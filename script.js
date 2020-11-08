394931e9046cf2ae6cc0ac010e3e54eb5fc9a91c
Gustavo Cotta <gmcotta34@gmail.com>
2020-11-07 23:45:19 -0300

    Adding game over

--- script.js
+++ script.js
@@ -44,9 +44,17 @@ function startGame() {
   if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
   if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
 
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+      clearInterval(game);
+      alert('Game over');
+    }
+  }
+
   createBG();
   createSnake();
   createFood();
+
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
   if (direction === 'right') snakeX += box;
