9bbf5650a60269005806ec43ce86e66fb400c2b1
Gustavo Cotta <gmcotta34@gmail.com>
2020-11-07 23:32:51 -0300

    Creating food

--- script.js
+++ script.js
@@ -5,27 +5,36 @@ let snake = [];
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
-}
-let direction = "right";
+};
+let direction = 'right';
+let food = {
+  x: Math.floor(Math.random() * 16) * box,
+  y: Math.floor(Math.random() * 16) * box,
+};
 
 function createBG() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
-}
+};
 
 function createSnake() {
   for (let i = 0; i < snake.length; i++) {
     context.fillStyle = 'green';
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
-}
+};
+
+function createFood() {
+  context.fillStyle = 'red';
+  context.fillRect(food.x, food.y, box, box);
+};
 
 function update(event) {
   if (event.keyCode === 37 && direction !== 'right') direction = 'left';
   if (event.keyCode === 38 && direction !== 'down') direction = 'up';
   if (event.keyCode === 39 && direction !== 'left') direction = 'right';
   if (event.keyCode === 40 && direction !== 'up') direction = 'down';
-}
+};
 
 document.addEventListener('keydown', update);
 
@@ -37,6 +46,7 @@ function startGame() {
 
   createBG();
   createSnake();
+  createFood();
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
   if (direction === 'right') snakeX += box;
@@ -49,6 +59,6 @@ function startGame() {
     y: snakeY,
   };
   snake.unshift(newHead);
-}
+};
 
 let game = setInterval(startGame, 100);
